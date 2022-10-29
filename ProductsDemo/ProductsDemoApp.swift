0e641ce198f1adb76b78d2143a4de847637a0dc1
Kuang Lin <slk11075@gmail.com>
2022-10-30 00:10:50 +0100

    Initial Commit

---
+++ ProductsDemo/ProductsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductsDemoApp.swift
+//  ProductsDemo
+//
+//  Created by Kris Lin on 2022/10/30.
+//
+
+import SwiftUI
+
+@main
+struct ProductsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
